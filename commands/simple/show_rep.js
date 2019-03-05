8e577b1c27973cc0e246ab1ce07a99529c40395b
Arjun Subhash <31234599+Extrone@users.noreply.github.com>
2019-03-05 20:59:42 +0530

    Don't Know
    I honestly don't know what I changed LOL, forgot to commit right away

--- commands/simple/show_rep.js
+++ commands/simple/show_rep.js
@@ -1,7 +1,6 @@
 const commando = require('discord.js-commando');
 const reputation = require("../../reputation.json");
 const Discord = require('discord.js');
-const fs = require("fs");
 
 class ShowRepCommand extends commando.Command
 {
@@ -25,33 +24,54 @@ class ShowRepCommand extends commando.Command
 
         let targetUser = message.guild.member(message.mentions.users.first());
         var targetRep = reputation[targetUser.id].rep;
-        var targetURL = '';
-        var targetColor = 'cccccc';
+        var tierURL = '';
+        var tierColor = 'cccccc';
+        var tierName = 'Common';
+        var tierDifference = '';
+        var nextTierURL = 'https://media.giphy.com/media/bLcUXdVYNDQrbQxywX/giphy.gif'
 
         if(targetRep >= commonRange && targetRep < unCommonRange)
         {
-            targetURL = 'https://media.giphy.com/media/1APjr0YOlz3ZfZBU5X/giphy.gif';
-            targetColor = 'cccccc';
+            tierURL = 'https://media.giphy.com/media/1APjr0YOlz3ZfZBU5X/giphy.gif';
+            tierColor = 'cccccc';
+            tierName = 'Common';
+            tierDifference = unCommonRange - targetRep + ' more rep need to advance to Un-Common Tier.';
+            nextTierURL = 'https://media.giphy.com/media/bLcUXdVYNDQrbQxywX/giphy.gif'
         }
         else if(targetRep >= unCommonRange && targetRep < rareRange)
         {
-            targetURL = 'https://media.giphy.com/media/bLcUXdVYNDQrbQxywX/giphy.gif';
-            targetColor = '11ff11';
+            tierURL = 'https://media.giphy.com/media/bLcUXdVYNDQrbQxywX/giphy.gif';
+            tierColor = '11ff11';
+            tierName = 'UnCommon';
+            tierDifference = rareRange - targetRep + ' more rep need to advance to Rare Tier.';
+            nextTierURL = 'https://media.giphy.com/media/vwjGwKxOn9YqmAcV26/giphy.gif'
+
         }
         else if(targetRep >= rareRange && targetRep < epicRange)
         {
-            targetURL = 'https://media.giphy.com/media/vwjGwKxOn9YqmAcV26/giphy.gif';
-            targetColor = '1111ff';
+            tierURL = 'https://media.giphy.com/media/vwjGwKxOn9YqmAcV26/giphy.gif';
+            tierColor = '1111ff';
+            tierName = 'Rare';
+            tierDifference = epicRange - targetRep + ' more rep need to advance to Epic Tier.';
+            nextTierURL = 'https://media.giphy.com/media/4N8Yg0TuzN9vO982po/giphy.gif'
+
         }
         else if(targetRep >= epicRange && targetRep < legendaryRange)
         {
-            targetURL = 'https://media.giphy.com/media/4N8Yg0TuzN9vO982po/giphy.gif';
-            targetColor = 'bb11ff';
+            tierURL = 'https://media.giphy.com/media/4N8Yg0TuzN9vO982po/giphy.gif';
+            tierColor = 'bb11ff';
+            tierName = 'Epic';
+            tierDifference = legendaryRange - targetRep + ' more rep need to advance to Legendary Tier.';
+            nextTierURL = 'https://media.giphy.com/media/1Bg7F8cpvaqXpKDIXe/giphy.gif'
+
         }
         else if(targetRep >= legendaryRange)
         {
-            targetURL = 'https://media.giphy.com/media/1Bg7F8cpvaqXpKDIXe/giphy.gif';
-            targetColor = 'ffff11';
+            tierURL = 'https://media.giphy.com/media/1Bg7F8cpvaqXpKDIXe/giphy.gif';
+            tierColor = 'ffff11';
+            tierName = 'Legendary';
+            tierDifference = 'You are at the most Advanced Tier :D';
+            nextTierURL = 'https://media.giphy.com/media/1Bg7F8cpvaqXpKDIXe/giphy.gif'
         }
 
         if(!targetUser){
@@ -64,10 +84,12 @@ class ShowRepCommand extends commando.Command
             };
         }
         let repEmbed = new Discord.RichEmbed()
-            .setTitle(targetUser.user.username + "'s Reputation")
-            .addField("Reputation", (reputation[targetUser.id].rep), false)
-            .setThumbnail(targetURL)
-            .setColor(targetColor)
+            .setTitle(targetUser.user.username)
+            .addField("Tier", tierName, false)
+            .addField("Reputation", targetRep, false)
+            .setThumbnail(tierURL)
+            .setColor(tierColor)
+            .setFooter(tierDifference, nextTierURL)
 
         message.channel.send(repEmbed);
     }
